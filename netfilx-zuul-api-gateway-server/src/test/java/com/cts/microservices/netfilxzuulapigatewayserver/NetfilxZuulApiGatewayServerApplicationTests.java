9466563331f536ae1484a2781cb430c32baca0e2
Srikanth Naidu <srikanth.naidu@sap.com>
2021-06-19 19:45:53 +0530

    zull and eureka commit

---
+++ netfilx-zuul-api-gateway-server/src/test/java/com/cts/microservices/netfilxzuulapigatewayserver/NetfilxZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.microservices.netfilxzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfilxZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
