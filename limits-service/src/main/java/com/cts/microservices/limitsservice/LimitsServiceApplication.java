c007521e15ecadc9b5f006dc49a806869aedd335
sri.kanth.naidu <sri.kanth.naidu@BDC8-LX-115BCYG.dir.svc.accenture.com>
2021-10-28 09:46:04 +0530

    Test commit

--- limits-service/src/main/java/com/cts/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/cts/microservices/limitsservice/LimitsServiceApplication.java
@@ -10,6 +10,7 @@ public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LimitsServiceApplication.class, args);
+		System.out.println("Test");
 	}
 
 }
